ddb0585716891e4d96fc435dc7f085cb8fde58e0
Sebastian Hamm <sebastian.hamm@bnet.at>
2025-06-23 18:26:35 +0200

    Update git tracking to respect .gitignore rules

---
+++ Backend/src/main/java/at/sebastianhamm/kapelle_eisenstadt/BlasmusikwebsiteApplication.java
@@ -0,0 +1,13 @@
+package at.sebastianhamm.kapelle_eisenstadt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlasmusikwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlasmusikwebsiteApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/at/sebastianhamm/kapelle_eisenstadt/BlasmusikwebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package at.sebastianhamm.kapelle_eisenstadt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlasmusikwebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
